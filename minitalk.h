c6ea2075595dca9957570d78968dc90727c87118
Kathrin Czichowsky <kczichow@1-B-15.42heilbronn.de>
2022-09-22 16:32:18 +0200

    Headerfile modified, but probably not necessary at all

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: kczichow <kczichow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 10:18:54 by kczichow          #+#    #+#             */
-/*   Updated: 2022/09/21 11:28:20 by kczichow         ###   ########.fr       */
+/*   Updated: 2022/09/22 14:30:39 by kczichow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,26 @@
 
 #include <signal.h>
 #include <unistd.h>
+#include <stdio.h>
+#include "libft.h"
 
-struct  sigaction
-{
-	union __sigaction_u __sigaction_u;  /* signal handler */
-    sigset_t sa_mask;               /* signal mask to apply */
-    int     sa_flags;               /* see signal options below */
-};
+// struct  sigaction
+// {
+// 	union __sigaction_u __sigaction_u;  /* signal handler */
+//     sigset_t sa_mask;               /* signal mask to apply */
+//     int     sa_flags;               /* see signal options below */
+// };
 
-union __sigaction_u {
-    void    (*__sa_handler)(int);
-    void    (*__sa_sigaction)(int, siginfo_t *, void *);
-};
+// union __sigaction_u {
+//     void    (*__sa_handler)(int);
+//     void    (*__sa_sigaction)(int, siginfo_t *, void *);
+// };
 
-     #define sa_handler      __sigaction_u.__sa_handler
-     #define sa_sigaction    __sigaction_u.__sa_sigaction
+//      #define sa_handler      __sigaction_u.__sa_handler
+//      #define sa_sigaction    __sigaction_u.__sa_sigaction
 
 
-     int
-     sigaction(int sig, const struct sigaction *restrict act, struct sigaction *restrict oact);
+//      int
+//      sigaction(int sig, const struct sigaction *restrict act, struct sigaction *restrict oact);
 
 #endif
